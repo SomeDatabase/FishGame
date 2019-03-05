5ae3e6be7d3666371eb058304179a0dd4526854d
CS Student <CSStudent@css-macbook-pro-5.local>
2019-03-05 16:01:18 -0500

    this is a commit message

---
+++ HintArrays.swift
@@ -0,0 +1,18 @@
+//
+//  HintArrays.swift
+//  DNRFishGame
+//
+//  Created by CS Student on 3/5/19.
+//  Copyright © 2019 LSSU. All rights reserved.
+//
+
+import Foundation
+//arrays containing hints for each species
+let aSalmHints = ["I have black spots on my sides, but none on my tail","You can find me in the St. Mary’s River, Lake Huron, or even the St. Clair river,"]
+let bTroutHints = ["I have a long body and a large mouth","I am the state fish of Michigan!","I lay up to 5000 eggs"]
+let chinSalmonHints = ["I am present in all 5 great lakes","I am known as the King","My primary source of food is alewives"]
+let cohoSalmonHints = ["I have small black spots on my back and tail","I can be caught in Lake Michigan at anytime of the year","I started the Great Lakes salmon fishery"]
+let sturgHints = ["I primarily eat insect larvae, crayfish, snails, clam, and leeches","My typical lifespan is 55 years","I have four barbels on my snout"]
+let lkTroutHints = ["I have light spots on my back and tail","I can be found in all 5 Great Lakes","I prefer cold water"]
+let muskHints = ["I prefer slightly cooler water, but I can withstand water of up to 90 F","My jaw is shaped like a duck’s bill","I am primarily located in Western Upper Peninsula"]
+let rnbwTroutHints = ["","",""]
