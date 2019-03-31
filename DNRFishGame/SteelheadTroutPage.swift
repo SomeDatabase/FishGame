90d31706536d9600f2dfac4fd4ed2606759dafd6
Student <student@macbook-pro-2>
2019-03-30 20:31:36 -0400

    Lots of changes to complete Storybook

---
+++ DNRFishGame/SteelheadTroutPage.swift
@@ -0,0 +1,84 @@
+//
+//  SteelheadTroutPage.swift
+//  DNRFishGame
+//
+//  Created by Student on 3/30/19.
+//  Copyright © 2019 LSSU. All rights reserved.
+//
+
+import UIKit
+
+class SteelheadTroutPage: UIPageViewController, UIPageViewControllerDelegate, UIPageViewControllerDataSource {
+    
+    lazy var orderedViewControllers: [UIViewController] = {
+        return [self.newVc(viewController: "Steel1"),
+                self.newVc(viewController: "Steel2"),
+                self.newVc(viewController: "Steel3"),
+                self.newVc(viewController: "Steel4"),
+                self.newVc(viewController: "Steel5"),
+                self.newVc(viewController: "Steel6"),]
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.dataSource = self
+        
+        if let firstViewController = orderedViewControllers.first {
+            setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
+        }
+        // Do any additional setup after loading the view.
+    }
+    
+    func newVc(viewController: String) -> UIViewController {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: viewController)
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else {
+            return orderedViewControllers.last
+        }
+        
+        guard orderedViewControllers.count > previousIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        
+        guard orderedViewControllers.count != nextIndex else {
+            return orderedViewControllers.first
+        }
+        
+        guard orderedViewControllers.count > nextIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[nextIndex]
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+
+
+}
