e822635a8a411722a4d8a00f2bab1d39b603d934
CS Student <CSStudent@css-macbook-pro-5.local>
2019-04-13 14:52:57 -0400

    updated the fish hints

--- DNRFishGame/ViewController.swift
+++ DNRFishGame/ViewController.swift
@@ -299,7 +299,7 @@ class FishPuzzle: UIViewController {
     func initFishData()
     {
         //arrays to handle the hints
-        let aSalmHints = ["I have black spots on my sides, but none on my tail","You can find me in the St. Mary’s River, Lake Huron, or even the St. Clair river,"]
+        let aSalmHints = ["I have black spots on my sides, but none on my tail","You can find me in the St. Mary’s River, Lake Huron, or even the St. Clair river,","I am one of the most prized game fish in the world"]
         let bTroutHints = ["I have a long body and a large mouth","I am the state fish of Michigan!","I lay up to 5000 eggs"]
         let brwnTroutHints = ["In the spring you’ll find me in shallow, rocky areas","The DNR rears me at the Harrieta and Oden State Fish Hatcheries","I prefer warmer weather"]
         let chinSalmonHints = ["I am present in all 5 great lakes","I am known as the King","My primary source of food is alewives"]
@@ -307,9 +307,9 @@ class FishPuzzle: UIViewController {
         let sturgHints = ["I primarily eat insect larvae, crayfish, snails, clam, and leeches","My typical lifespan is 55 years","I have four barbels on my snout"]
         let lkTroutHints = ["I have light spots on my back and tail","I can be found in all 5 Great Lakes","I prefer cold water"]
         let muskHints = ["I prefer slightly cooler water, but I can withstand water of up to 90 F","My jaw is shaped like a duck’s bill","I am primarily located in Western Upper Peninsula"]
-        let rnbwTroutHints = ["I have a pink stripe on my body","I primarily live in clear-water lakes throughout Michigan"]
-        let stlhdTroutHints = ["I am born in rivers, but spend most of my life in the great lakes","My life expectancy is 4 to 6 years"]
-        let wllyHints = ["I spawn in rock shoals","I am a popular choice for sport fishing because I feed all year round"]
+        let rnbwTroutHints = ["I have a pink stripe on my body","I primarily live in clear-water lakes throughout Michigan","When I grow older, I eat smallfish, but I primarily eat insects"]
+        let stlhdTroutHints = ["I am born in rivers, but spend most of my life in the great lakes","My life expectancy is 4 to 6 years","You can find me in the summer time in scumlines"]
+        let wllyHints = ["I spawn in rock shoals","I am a popular choice for sport fishing because I feed all year round","I am a greedy predator. I eat many small kinds of fish, such as a bass, perch, sunfish, pike, and trout"]
         var fishNum = 0
         var fish2 = 0
         var fish3 = 0
